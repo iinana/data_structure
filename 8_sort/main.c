c4f06ca75f18b1a6c50d6026fa8dea67254e90e1
ina <jia3527@gmail.com>
2023-10-31 17:37:50 +0200

    merge sort

---
+++ 8_sort/main.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Correct Usage : [Program] [Input File] [Output File]\n");
+        return 1;
+    }
+
+    FILE *inFile = fopen(argv[1], "r");
+    FILE *outFile = fopen(argv[2], "w");
+    if (!inFile || !outFile) {
+        printf("Failed to Open File\n");
+        return 1;
+    }
+
+    char c;
+    while ((c = fgetc(inFile)) != EOF) {
+        if (!isalpha(c)) continue;
+        if (c == 'M') {
+            int len;
+            fscanf(inFile, "%d", &len);
+
+            int *nums = (int *)malloc(sizeof(int) * len);
+            for (int i = 0; i < len; i++) fscanf(inFile, "%d", &nums[i]);
+
+            if (mergeSort(nums, 0, len-1)) {
+                fputc('M', outFile);
+                for (int i = 0; i < len; i++) fprintf(outFile, " %d", nums[i]); 
+            } else fputs("Merge Sort Error", outFile);
+            free(nums);
+        }
+        // else if (c == 'Q') mergeSort();
+        else fputs("Function Error", outFile);
+        fputc('\n', outFile);
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+}
---
+++ 8_sort/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int mergeSort(int *nums, int start, int end);
+void merge(int *nums, int start, int mid, int end);
