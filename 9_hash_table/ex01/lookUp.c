7dc4dc6e673031f0f5d2755599ba9a1dc7a09607
ina <jia3527@gmail.com>
2023-11-01 15:43:35 +0200

    check repeated value

---
+++ 9_hash_table/ex01/lookUp.c
@@ -0,0 +1,27 @@
+#include "../hash/hash.h"
+
+bool isRepeated(Hash **hashTable) {
+    for (int i = 0; i < HASH_LEN; i++) {
+        if (hashTable[i]->value) {
+            Hash *cur = hashTable[i];
+            while (cur->next) {
+                int q = cur->value;
+                Hash *n = cur->next;
+                while (n) {
+                    if (q == n->value) return true;
+                    n = n->next;
+                }
+                cur = cur->next;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    printf("Enter the Integers: ");
+    Hash **hashTable = makeHashTable();
+
+    if (isRepeated(hashTable)) printf("Repeated Value : True\n");
+    else printf("Repeated Value : False\n");
+}
